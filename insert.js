5852e731a7785fec46139afb21587677ffd6b137
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-06-28 11:55:13 -0300

    Update insert.js

--- insert.js
+++ insert.js
@@ -1,3 +1,19 @@
 const frm = document.querySelector("form");
 const resp = document.querySelector("h3");
 
+function insertString(str, insert, index){
+    const charArray = str.split("");
+    let insertCount = 0;
+
+    for(let i = 0; i <= str.length; i++){
+        //console.log(str[i]);
+
+        if(index.includes(i)){
+            charArray.splice(i + insertCount, 0, insert);
+            insertCount++
+        }
+    }
+
+    return charArray.join("");
+}
+console.log(insertString("CELEBS TÁ LOCO", "O ", [0]))
